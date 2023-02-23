894d9272f31f04b402b99c867c908480e0598d5a
Tudor <tudor.onceanu@gmail.com>
2023-02-23 10:10:18 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { Configuration, OpenAIApi } from 'openai';
+
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const configuration = new Configuration({
+        organization: "org-NSgm9uRMro8cjfkkzBEiPWir",
+        apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+    });
+
+    const openai = new OpenAIApi(configuration);
+
+    const [prompt, setPrompt] = useState('')
+    const [result, setResult] = useState('')
+    const [loading, setLoading] = useState(false);
+
+    const handleClick = async() => {
+        setLoading(true)
+        try {
+            const response = await openai.createCompletion({
+                model: 'text-davinci-003',
+                prompt: prompt,
+                temperature: 0.5,
+                max_tokens: 1000,
+            })
+            setResult(response.data.choices[0].text)
+        } catch (error) {
+            console.log(error)
+        }
+        setLoading(false)
+    }
+
+    return ( <
+        main className = "main" >
+        <
+        h1 > TUDOR 's CHAT GPT</h1> <
+        textarea type = 'text'
+        value = { prompt }
+        onChange = { e => setPrompt(e.target.value) }
+        placeholder = 'Write your prompt..'
+        className = 'textarea' >
+        <
+        /textarea> <
+        button onClick = { handleClick }
+        disabled = { loading || prompt.length === 0 }
+        className = 'btn' >
+        { loading ? 'Generating...' : 'Generate' } <
+        /button> <
+        pre className = 'result' > { result } < /pre> <
+        /main>
+    );
 }
 
 export default App;
